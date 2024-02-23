885f313505f8bb4ae352f396e9f91c628ece94d3
Kolade Akintoye <akintoyekolade1@gmail.com>
2024-02-23 15:18:47 +0800

    Update Files

---
+++ admin/index.php
@@ -0,0 +1,3 @@
+<?php
+    include('php-project/header.php');
+?>
---
+++ php-project/header.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="We have a wide collection of hoodies, sweatshirts, t-shirts, and clothing accessories">
+    <meta name="author" content="Holy-Esprit Designs">
+    <meta name="keywords" content="hoodies, sweatshirts, t-shirts, clothing accessories">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/images/HED 1 - Favicon.svg" type="image/HED-icon">
+    <title>Holy-Esprit Designs</title>
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <link rel="stylesheet" href="assets/css/owl-carousel.css">
+
+    <link rel="stylesheet" href="assets/css/lightbox.css">
+  </head>
+    
+  <body>
+    
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+    
+    
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="index.html" class="logo">
+                            <img src="assets/images/HED - Black.png">
+                        </a>
+                        <!-- ***** Logo End ***** -->
+
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="#top" class="active">HOME</a></li>
+                            <li class="nav-item"><a href="products.html">SHOP ALL</a></li>
+                            <li class="scroll-to-section"><a href="#Hoodie">HOODIES</a></li>
+                            <li class="scroll-to-section"><a href="#sweatshirt">SWEATSHIRTS</a></li>
+                            <li class="scroll-to-section"><a href="#tshirt">T-SHIRTS</a></li>
+                            <li class="scroll-to-section"><a href="#accessories">ACCESSORIES</a></li>                        
+                            <li class="submenu">
+                                <a href="javascript:;">ABOUT US</a>
+                                <ul>
+                                    <li><a href="about.html">OUR MISSION</a></li>                                    
+                                    <li><a href="contact.html">CONTACT US</a></li>                                
+                                    <li class="scroll-to-section"><a href="#mission">MISSION</a></li>
+                                </ul>
+                            </li>
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header Area End ***** -->
