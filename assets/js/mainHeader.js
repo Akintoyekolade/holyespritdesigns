b25b3a4e3660612f597fbed3e7a9902b10cad622
Kolade Akintoye <akintoyekolade1@gmail.com>
2024-02-25 05:45:56 +0800

    Update Files

--- assets/js/mainHeader.js
+++ assets/js/mainHeader.js
@@ -52,3 +52,15 @@ class MainHeader extends HTMLElement {
 
 customElements.define('my-header', MainHeader)
 
+// navigation.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const currentLocation = location.href;
+    const navLinks = document.querySelectorAll(".nav a");
+
+    navLinks.forEach(link => {
+        if (link.href === currentLocation) {
+            link.classList.add("active");
+        }
+    });
+});
